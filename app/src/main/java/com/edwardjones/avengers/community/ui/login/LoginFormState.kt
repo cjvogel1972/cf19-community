ff59e7d2eac936f1900a664aa8e4b564c1ec6bf8
Chris Vogel <cjvogel1972@gmail.com>
2019-09-19 18:14:35 -0500

    Add initial login screen

---
+++ app/src/main/java/com/edwardjones/avengers/community/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.edwardjones.avengers.community.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
