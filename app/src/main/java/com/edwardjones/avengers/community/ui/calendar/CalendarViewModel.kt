41e00eb5eea2fbeb41451ee81c35bf3aa3700acd
Nathan <nathan.e.barron@gmail.com>
2019-09-19 18:54:31 -0500

    Initial Calendar skel change

--- app/src/main/java/com/edwardjones/avengers/community/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/edwardjones/avengers/community/ui/calendar/CalendarViewModel.kt
@@ -1,13 +1,13 @@
-package com.edwardjones.avengers.community.ui.slideshow
+package com.edwardjones.avengers.community.ui.calendar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class CalendarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is calendar Fragment"
     }
     val text: LiveData<String> = _text
 }
