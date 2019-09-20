8b1e65705896a3b01d389d75c12d465eb410990b
Chris Vogel <cjvogel1972@gmail.com>
2019-09-20 01:26:24 -0500

    Chat room work

--- app/src/main/java/com/edwardjones/avengers/community/ui/chatlobby/ChatLobbyFragment.kt
+++ app/src/main/java/com/edwardjones/avengers/community/ui/chatlobby/ChatLobbyFragment.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.edwardjones.avengers.community.R
+import com.edwardjones.avengers.community.ui.chatroom.ChatRoomFragment
 
 class ChatLobbyFragment : Fragment() {
 
@@ -112,11 +113,25 @@ class ChatLobbyFragment : Fragment() {
         params.columnSpec = GridLayout.spec(column)
         button.layoutParams = params
         button.scaleType = ImageView.ScaleType.FIT_CENTER
+        button.setOnClickListener {
+            val id = it.id - 100
+            val roomName = chatLobbyViewModel.roomNames[id]
+            val apptFragment = ChatRoomFragment(roomName)
+            replaceFragment(apptFragment)
+
+        }
         gridLayout.addView(button)
     }
 
     fun dpToPx(dp: Int, context: Context?): Int {
         val density = context?.getResources()?.getDisplayMetrics()?.density
         return Math.round(dp.toFloat() * density!!)
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace((view!!.parent as ViewGroup).id, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/edwardjones/avengers/community/ui/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/edwardjones/avengers/community/ui/chatroom/ChatRoomFragment.kt
@@ -1,18 +1,21 @@
 package com.edwardjones.avengers.community.ui.chatroom
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.edwardjones.avengers.community.R
+import kotlinx.android.synthetic.main.fragment_chat_room.*
 
-class ChatRoomFragment : Fragment() {
+class ChatRoomFragment(roomName: String) : Fragment() {
 
     private lateinit var chatRoomViewModel: ChatRoomViewModel
+    private val roomName: String = roomName
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +25,19 @@ class ChatRoomFragment : Fragment() {
         chatRoomViewModel =
             ViewModelProviders.of(this).get(ChatRoomViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_chat_room, container, false)
-        val textView: TextView = root.findViewById(R.id.text_share)
-        chatRoomViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        val sendButton = root.findViewById<ImageButton>(R.id.send_message)
+        val editText = root.findViewById<EditText>(R.id.editText)
+
+        Log.i("CHAT", "room name = $roomName")
+        sendButton.setOnClickListener {
+            val message = editText.text.toString()
+            if (message.isNotEmpty()) {
+                // send message on websocket
+                editText.text.clear()
+            }
+        }
+
         return root
     }
 }
